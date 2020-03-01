a5323a35939fdeb11dedd98aae0e0242aefdb952
Atekawere <jacintawere2@gmail.com>
2020-03-01 22:31:22 +0300

    worked on licence part

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 $(document).ready(function(){
-    $("form#delivery").submit(function(event){
+    $("form#delivery").checkout(function(event){
         var name = $("input#name").val();
         var location = $("input#location").val();
-        var number = $("input#numer").val();
+        var number = $("input#number").val();
         alert("Hello" + name + "we have received your order and will be delivered to you" + location + "in an hour thankyou for chosing home of pizzerians")
     });
 });
